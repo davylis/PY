65880ea9257de9a61872e7eaf7265a242668a679
Liisa Davydov <liisamuttonen@gmail.com>
2025-01-29 21:22:02 +0200

    Add exercise

---
+++ Chapter_6/strings_slicing_4.py
@@ -0,0 +1,9 @@
+def strings_slicing_4():
+    usr_str=input("Enter a string: ")
+    #palidrome checking(if is same reversed)
+    if usr_str == usr_str[::-1]:
+        print("Yes")
+    else:
+        print("No")
+
+strings_slicing_4()
