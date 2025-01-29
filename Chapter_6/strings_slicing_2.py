b484d4ec967d7cda8053f291cabb3b7e3a66d8ac
Liisa Davydov <liisamuttonen@gmail.com>
2025-01-29 21:13:14 +0200

    Add exercise

---
+++ Chapter_6/strings_slicing_2.py
@@ -0,0 +1,9 @@
+def strings_slicing_2():
+    usr_str=input("Enter a string: ")
+    if len(usr_str) < 2:
+        print("Too short string")
+    else:
+        print(usr_str[-2])
+
+
+strings_slicing_2()
