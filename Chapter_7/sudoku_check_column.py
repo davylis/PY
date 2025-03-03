06b8ab788b85a895ad7ac8d7ff8e095092179e57
Liisa Davydov <liisamuttonen@gmail.com>
2025-03-03 15:04:56 +0200

    add exercise

--- Chapter_7/sudoku_check_column.py
+++ Chapter_7/sudoku_check_column.py
@@ -10,17 +10,21 @@ def column_ok(sudoku, index):
     return True
 
 def main():
-sudoku = [
-[9, 0, 0, 0, 8, 0, 3, 0, 0],
-[2, 0, 0, 2, 5, 0, 7, 0, 0],
-[0, 2, 0, 3, 0, 0, 0, 0, 4],
-[2, 9, 4, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 7, 3, 0, 5, 6, 0],
-[7, 0, 5, 0, 6, 0, 4, 0, 0],
-[0, 0, 7, 8, 0, 3, 9, 0, 0],
-[0, 0, 1, 0, 0, 0, 0, 0, 3],
-[3, 0, 0, 0, 0, 0, 0, 0, 2]
-]
-print(column_ok(sudoku, column_index=0)) # False
-print(column_ok(sudoku, column_index=1)) # True
-print(column_ok(sudoku, column_index=8)) 
+    sudoku = [
+    [9, 0, 0, 0, 8, 0, 3, 0, 0],
+    [2, 0, 0, 2, 5, 0, 7, 0, 0],
+    [0, 2, 0, 3, 0, 0, 0, 0, 4],
+    [2, 9, 4, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 7, 3, 0, 5, 6, 0],
+    [7, 0, 5, 0, 6, 0, 4, 0, 0],
+    [0, 0, 7, 8, 0, 3, 9, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 3],
+    [3, 0, 0, 0, 0, 0, 0, 0, 2]
+    ]
+    print(column_ok(sudoku, index=0)) # False
+    print(column_ok(sudoku, index=1)) # True
+    print(column_ok(sudoku, index=8)) 
+
+
+if __name__ == "__main__":
+    main()
