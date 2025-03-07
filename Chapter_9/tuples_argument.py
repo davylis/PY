d5eaa22cf5a9842d0e51093d226b18a2ce4e3750
Liisa Davydov <liisamuttonen@gmail.com>
2025-03-07 11:19:11 +0200

    Add exercises

---
+++ Chapter_9/tuples_argument.py
@@ -0,0 +1,7 @@
+def print_player(player):
+    print(f'{player[1]} ({player[0]}), {player[2]} goals')
+
+def main():
+    p = ("Hawks", "Jennifer", 10)
+    print_player(p)
+main()
---
+++ Chapter_9/tuples_first.py
@@ -0,0 +1,4 @@
+def tuples_first():
+    i = int(input("Enter an int: "))
+    f = float(input("Enter a float:"))
+    print((i, f))
